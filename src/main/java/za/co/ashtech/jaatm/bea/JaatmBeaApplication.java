430ce4f9e7eca01269075fdc88e18887054479c6
Ashwin <ashwin.witbooi@gmail.com>
2025-12-19 20:19:16 +0200

    initial commit base bones project

---
+++ src/main/java/za/co/ashtech/jaatm/bea/JaatmBeaApplication.java
@@ -0,0 +1,13 @@
+package za.co.ashtech.jaatm.bea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaatmBeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaatmBeaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/za/co/ashtech/jaatm/bea/JaatmBeaApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.ashtech.jaatm.bea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaatmBeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
