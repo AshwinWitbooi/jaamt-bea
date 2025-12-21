2e3e93a46ec12b3372f64f11915a804928e9ccfe
Ashwin <ashwin.witbooi@gmail.com>
2025-12-21 14:15:14 +0200

    add H2 in memory local dev initialization script to create JAATM-USER table and data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO JAAT_USER (JUID, FIRSTNAME, LASTNAME) VALUES
+('JUID001', 'Aarav', 'Sharma'),
+('JUID002', 'Diya', 'Verma'),
+('JUID003', 'Rohan', 'Gupta'),
+('JUID004', 'Ananya', 'Singh'),
+('JUID005', 'Kunal', 'Mehta'),
+('JUID006', 'Priya', 'Malhotra'),
+('JUID007', 'Vikram', 'Reddy'),
+('JUID008', 'Neha', 'Kapoor'),
+('JUID009', 'Arjun', 'Patel'),
+('JUID010', 'Sneha', 'Iyer');
