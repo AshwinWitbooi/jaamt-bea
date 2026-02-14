a1f1258b6ce1577f191eb69fe70ab37434c139aa
Ashwin <ashwin.witbooi@gmail.com>
2026-02-14 15:57:24 +0200

    remove commented code and fix Exception handler

--- src/main/java/za/co/ashtech/jaatm/bea/aop/GlobalApiErrorControllerAdvice.java
+++ src/main/java/za/co/ashtech/jaatm/bea/aop/GlobalApiErrorControllerAdvice.java
@@ -1,11 +1,13 @@
 package za.co.ashtech.jaatm.bea.aop;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import jakarta.servlet.http.HttpServletRequest;
 import za.co.ashtech.jaatm.bea.dto.ApiErrorResponse;
+import za.co.ashtech.jaatm.bea.util.JaatmConstants;
 import za.co.ashtech.jaatm.bea.util.UserNotFoundException;
 
 @RestControllerAdvice
@@ -25,39 +27,20 @@ public class GlobalApiErrorControllerAdvice {
         return ResponseEntity.status(ex.getStatus()).body(response);
     }
 
-//    @ExceptionHandler(MethodArgumentNotValidException.class)
-//    public ResponseEntity<ApiErrorResponse> handleValidationException(
-//            MethodArgumentNotValidException ex,
-//            HttpServletRequest request) {
-//
-//        String message = ex.getBindingResult()
-//                .getFieldErrors()
-//                .get(0)
-//                .getDefaultMessage();
-//
-//        ApiErrorResponse response = new ApiErrorResponse(
-//                HttpStatus.BAD_REQUEST,
-//                message,
-//                "VALIDATION_ERROR",
-//                request.getRequestURI()
-//        );
-//
-//        return ResponseEntity.badRequest().body(response);
-//    }
-//
-//    @ExceptionHandler(Exception.class)
-//    public ResponseEntity<ApiErrorResponse> handleUnhandledException(
-//            Exception ex,
-//            HttpServletRequest request) {
-//
-//        ApiErrorResponse response = new ApiErrorResponse(
-//                HttpStatus.INTERNAL_SERVER_ERROR,
-//                "Unexpected error occurred",
-//                "INTERNAL_ERROR",
-//                request.getRequestURI()
-//        );
-//
-//        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
-//    }
+
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiErrorResponse> handleUnhandledException(
+            Exception ex,
+            HttpServletRequest request) {
+
+        ApiErrorResponse response = new ApiErrorResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                "Unexpected error occurred. Please contact support team.",
+                JaatmConstants.ERROR_CODE_100
+        );
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+    }
 }
 
