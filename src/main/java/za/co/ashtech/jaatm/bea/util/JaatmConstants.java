d98f8026a492ddef94031fee28043bd0f52da3d8
Ashwin <ashwin.witbooi@gmail.com>
2026-02-15 15:44:22 +0200

    add private constructor

--- src/main/java/za/co/ashtech/jaatm/bea/util/JaatmConstants.java
+++ src/main/java/za/co/ashtech/jaatm/bea/util/JaatmConstants.java
@@ -2,6 +2,8 @@ package za.co.ashtech.jaatm.bea.util;
 
 public class JaatmConstants {
 	
+	private JaatmConstants() {}
+	
 	public static final String ERROR_CODE_1 = "JAER-1"; // USER NOT FOUND
 	public static final String ERROR_CODE_100 = "JAER-100"; // GENERAL EXCEPTION OCCURED AND HAS TO BE INVESTIGATED
 
